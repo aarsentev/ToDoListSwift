07457aeae7e8983a50ab38e6ee1a2c55df63c788
Aleksandr Arsentev <arsenntv@gmail.com>
2025-12-03 15:37:04 +0800

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,20 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Alex Arsentev on 2025-12-03.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
