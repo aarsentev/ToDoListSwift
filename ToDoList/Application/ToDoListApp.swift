90ed552027052a2c9141d59cc2566f3ad36b0db1
Aleksandr Arsentev <arsenntv@gmail.com>
2025-12-04 18:40:14 +0800

    Cleanup

--- ToDoList/Application/ToDoListApp.swift
+++ ToDoList/Application/ToDoListApp.swift
@@ -13,8 +13,7 @@ struct ToDoListApp: App {
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
-                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+            ToDoListBuilder.build()
         }
     }
 }
