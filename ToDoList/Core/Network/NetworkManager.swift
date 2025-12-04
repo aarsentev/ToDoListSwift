3cc1c5577a960e18c3ee7c2201b77394ee4c0334
Aleksandr Arsentev <arsenntv@gmail.com>
2025-12-04 16:41:58 +0800

    Network manager added

---
+++ ToDoList/Core/Network/NetworkManager.swift
@@ -0,0 +1,75 @@
+//
+//  NetworkManager.swift
+//  ToDoList
+//
+//  Created by Alex Arsentev on 2025-12-04.
+//
+
+import Foundation
+
+final class NetworkManager {
+    // MARK: - NetworkError
+    enum NetworkError: Error {
+        case invalidURL
+        case noData
+        case decodingError
+        case serverError(statusCode: Int)
+    }
+    
+    static let shared = NetworkManager()
+    private let urlSession: URLSession
+    
+    private init(urlSession: URLSession = .shared) {
+        self.urlSession = urlSession
+    }
+    
+    func get<T: Decodable>(
+        url: String,
+        completion: @escaping (Result<T, NetworkError>) -> Void
+    ) {
+        guard let url = URL(string: url) else {
+            DispatchQueue.main.async {
+                completion(.failure(.invalidURL))
+            }
+            return
+        }
+        
+        let task = urlSession.dataTask(with: url) { data, response, error in
+            if error != nil {
+                DispatchQueue.main.async {
+                    completion(.failure(.noData))
+                }
+                return
+            }
+            
+            if let httpResponse = response as? HTTPURLResponse,
+               !(200...299).contains(httpResponse.statusCode) {
+                DispatchQueue.main.async {
+                    completion(.failure(.serverError(statusCode: httpResponse.statusCode)))
+                }
+                return
+            }
+            
+            guard let data = data else {
+                DispatchQueue.main.async {
+                    completion(.failure(.noData))
+                }
+                return
+            }
+            
+            do {
+                let decoder = JSONDecoder()
+                let result = try decoder.decode(T.self, from: data)
+                DispatchQueue.main.async {
+                    completion(.success(result))
+                }
+            } catch {
+                DispatchQueue.main.async {
+                    completion(.failure(.decodingError))
+                }
+            }
+        }
+        
+        task.resume()
+    }
+}
