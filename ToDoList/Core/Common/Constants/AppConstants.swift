82939eb81503aaae1c8cec75b2772bdea744d2d9
Aleksandr Arsentev <arsenntv@gmail.com>
2025-12-04 16:05:46 +0800

    Constants file added

---
+++ ToDoList/Core/Common/Constants/AppConstants.swift
@@ -0,0 +1,27 @@
+//
+//  AppConstants.swift
+//  ToDoList
+//
+//  Created by Alex Arsentev on 2025-12-04.
+//
+
+import Foundation
+
+enum AppConstants {
+    // MARK: - API
+    enum API {
+        static let baseURL = "https://dummyjson.com"
+        static let todosEndpoint = "/todos"
+        static let todosFullURL = baseURL + todosEndpoint
+    }
+    
+    // MARK: - UserDefaults Keys
+    enum UserDefaultsKeys {
+        static let isFirstLaunchCompleted = "isFirstLaunchCompleted"
+    }
+    
+    // MARK: - CoreData
+    enum CoreData {
+        static let containerName = "ToDoList"
+    }
+}
