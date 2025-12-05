73dc80940a5d6dcfe87827bff9ba3a7cc00312ee
Aleksandr Arsentev <arsenntv@gmail.com>
2025-12-05 13:32:29 +0800

    UI tests

--- ToDoListUITests/ToDoListUITests.swift
+++ ToDoListUITests/ToDoListUITests.swift
@@ -2,40 +2,34 @@
 //  ToDoListUITests.swift
 //  ToDoListUITests
 //
-//  Created by Alex Arsentev on 2025-12-03.
+//  Created by Alex Arsentev on 2025-12-05.
 //
 
 import XCTest
 
 final class ToDoListUITests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
+    
+    var app: XCUIApplication!
+    
+    override func setUp() {
+        super.setUp()
         continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    @MainActor
-    func testExample() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
+        app = XCUIApplication()
         app.launch()
-
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
-
-    @MainActor
-    func testLaunchPerformance() throws {
-        // This measures how long it takes to launch your application.
-        measure(metrics: [XCTApplicationLaunchMetric()]) {
-            XCUIApplication().launch()
-        }
+    
+    func testAppLaunches() {
+        XCTAssertTrue(app.exists)
+    }
+    
+    func testSearchBarExists() {
+        let searchField = app.searchFields.firstMatch
+        XCTAssertTrue(searchField.exists)
+    }
+    
+    func testCanTapSearchBar() {
+        let searchField = app.searchFields.firstMatch
+        searchField.tap()
+        XCTAssertTrue(searchField.exists)
     }
 }
